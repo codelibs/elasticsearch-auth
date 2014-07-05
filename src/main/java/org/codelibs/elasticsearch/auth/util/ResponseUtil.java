8b04545f6fb534620c235ce74de4b20c3a896934
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-07-06 07:23:17 +0900

    fix #8

--- src/main/java/org/codelibs/elasticsearch/auth/util/ResponseUtil.java
+++ src/main/java/org/codelibs/elasticsearch/auth/util/ResponseUtil.java
@@ -6,12 +6,11 @@ import org.codelibs.elasticsearch.auth.AuthException;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.rest.BytesRestResponse;
 import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestStatus;
-import org.elasticsearch.rest.XContentRestResponse;
-import org.elasticsearch.rest.XContentThrowableRestResponse;
-import org.elasticsearch.rest.action.support.RestXContentBuilder;
 
 public class ResponseUtil {
     private static final ESLogger logger = Loggers
@@ -29,21 +28,18 @@ public class ResponseUtil {
             final RestChannel channel, final RestStatus status,
             final String... args) {
         try {
-            final XContentBuilder builder = RestXContentBuilder
-                    .restContentBuilder(request);
+            final XContentBuilder builder = JsonXContent.contentBuilder();
             builder.startObject();
             builder.field("status", status.getStatus());
             for (int i = 0; i < args.length; i += 2) {
                 builder.field(args[i], args[i + 1]);
             }
             builder.endObject();
-            channel.sendResponse(new XContentRestResponse(request, status,
-                    builder));
+            channel.sendResponse(new BytesRestResponse(status, builder));
         } catch (final IOException e) {
             logger.error("Failed to send a response.", e);
             try {
-                channel.sendResponse(new XContentThrowableRestResponse(request,
-                        e));
+                channel.sendResponse(new BytesRestResponse(channel, e));
             } catch (final IOException e1) {
                 logger.error("Failed to send a failure response.", e1);
             }
